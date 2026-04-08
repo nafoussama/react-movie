f05f2b2b20d7f975ce5b75544c2bb5108e492919
oussama <nafouroussama@gmail.com>
2026-04-08 12:15:10 +0100

    update component with new logic and JSX

--- src/components/ListMovie.jsx
+++ src/components/ListMovie.jsx
@@ -5,8 +5,12 @@ import { Heart } from "lucide-react";
 const ListMovie = (props) => {
     const movie = props.moviesList;
     const [isLiked, setIsLiked] = useState(false);
+    const [likeCounter, setLikeCounter] = useState(0);
+
     const handleLike = () => {
         setIsLiked(!isLiked);
+        setLikeCounter((prev) => isLiked ? prev - 1 : prev + 1)
+        props.totalLikes((prev) => isLiked ? prev - 1 : prev + 1);
     }
     return (
         <section className="card">
@@ -14,24 +18,30 @@ const ListMovie = (props) => {
                 <p className="card-categorie" style={{ position: 'absolute', top: 10, left: 10, backgroundColor: movie.published ? 'green' : 'red' }}>
                     {movie.published ? 'published' : 'comming soon'}
                 </p>
-                <Heart
-                    onClick={handleLike}
-                    size={28}
-                    color={isLiked ? 'red' : 'white'}
-                    fill={isLiked ? 'red' : 'white'}
-                    style={{
-                        position: 'absolute', top: 10, right: 10, transition: "transform 0.2s ease",
-                        transform: isLiked ? "scale(1.2)" : "scale(1)"
-                    }}
-                />
 
                 <img src={movie.poster} alt={movie.title} onError={(e) => {
                     e.target.src = "https://placehold.co/280x340?text=No+Poster";
                 }} />
-
             </div>
             <div className="card-information">
                 <h4 className="card-title">{movie.title}</h4>
+                <div style={{
+                    position: 'absolute',
+                    top: 10,
+                    right: 10,
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: 4,
+                    zIndex: 10
+                }}>
+                    <Heart
+                        onClick={handleLike}
+                        size={28}
+                        color={isLiked ? 'red' : 'black'}
+                        fill={isLiked ? 'red' : 'none'}
+                    />
+                    <span>({likeCounter})</span>
+                </div>
                 <p className="card-description">{movie.description}</p>
                 <p className="card-rating">{movie.rating}/10</p>
                 <div className='bottom'>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import ListMovie from "./ListMovie"
 
 const MovieCard = (props) => {
+    const [likes, setLikes] = useState(0);
     const movie = props.movies;
+
+    const handleTotalLikes = () => {
+        setLikes(() => likes + 1);
+    }
     return (
-        <div className="movies">
-            {movie.map((ele, idx) => (
-                <ListMovie key={idx} moviesList={ele} />
-            ))}
-        </div>
+        <>
+            <div className="header-card">
+                <p className="total-like">Total of likes : {likes} </p>
+                <button className="btn-filter">Filter : { } </button>
+            </div>
+            <div className="movies">
+                {movie.map((ele, idx) => (
+                    <ListMovie key={idx} moviesList={ele} totalLikes={handleTotalLikes} />
+                ))}
+            </div>
+        </>
+
     )
 }
 
